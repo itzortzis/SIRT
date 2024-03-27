8342e1340d548e8cdff0465ad004d6767ef1d8f1
itzortzis <i.n.tzortzis@gmail.com>
2024-03-27 22:34:16 +0200

    Update: add get_cur_ip_addr function

--- server_info_report.sh
+++ server_info_report.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-ip_suffix=147.102.
-ifconfig_output=$(ifconfig | grep $ip_suffix)
 
-for word in $ifconfig_output; 
-do 
-    if [[ $word == $ip_suffix* ]] && [[ $word != *.255 ]];
-    then
-        echo $word; 
-    fi
-done
+get_cur_ip_addr()
+{
+    ip_suffix=147.102.
+    ifconfig_output=$(ifconfig | grep $ip_suffix)
+
+    for token in $ifconfig_output; 
+    do 
+        if [[ $token == $ip_suffix* ]] && [[ $token != *.255 ]];
+        then
+            ip_addr=$token; 
+        fi
+    done
+}
+
+ip_addr=xxx.xxx.xxx.xxx
+get_cur_ip_addr
+
+echo $ip_addr
