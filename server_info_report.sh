a1c19b2ad82a16396b16fa9baaf930f5026fe111
itzortzis <i.n.tzortzis@gmail.com>
2024-04-02 16:00:47 +0300

    Add: get_email_info function. Working version

--- server_info_report.sh
+++ server_info_report.sh
@@ -25,6 +25,41 @@ get_known_ip_addr()
     done < "$infile"
 }
 
+
+get_mail_info()
+{
+    infile=./messages.txt
+    i=0
+    IFS=$'\n' 
+    while read -r line
+    do
+        if [[ $i == 0 ]];
+        then
+            mail_sub="$line"
+        fi
+        if [[ $i == 1 ]];
+        then
+            mail_body="$line"
+        fi
+        i=$(( i + 1 ))
+    done < "$infile"
+}
+
+
+update_ip_addr()
+{
+    echo $1 > known_ip.txt
+}
+
+send_report()
+{
+    subject=$1
+    body=$2
+    echo $subject
+    echo $body
+    echo $body | mail -A known_ip.txt -s $subject i.n.tzortzis@gmail.com
+}
+
 ip_addr=xxx.xxx.xxx.xxx
 get_cur_ip_addr
 
@@ -33,7 +68,14 @@ echo $ip_addr
 get_known_ip_addr
 echo $known_ip
 
-# if [[ $ip_addr == $known_ip ]];
-# then
-#     echo "Yeees"
-# fi
+get_mail_info
+
+
+if [[ $ip_addr == $known_ip ]];
+then
+    echo "Yeees"
+else
+    update_ip_addr $ip_addr
+    send_report $mail_sub $mail_body
+fi
+
